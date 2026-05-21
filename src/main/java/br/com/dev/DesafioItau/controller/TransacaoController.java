ac4ca6127c60527949b4fde9c35ebfc093995b18
KauanFM <kauanmendes.5@hotmail.com>
2026-05-21 17:53:36 -0300

    Implementando o endpoint DELETE /transacao

--- src/main/java/br/com/dev/DesafioItau/controller/TransacaoController.java
+++ src/main/java/br/com/dev/DesafioItau/controller/TransacaoController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,15 @@ public class TransacaoController {
         transacaoService.salvarTransacao(entity);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+        
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTransacoes() {
+
+        transacaoService.deletarTransacoes();
+
+        return ResponseEntity.status(HttpStatus.OK).build();
         
     }
     
