b51acc6bda70bdbb66a7fc4e8656bec0dd495c70
KauanFM <kauanmendes.5@hotmail.com>
2026-05-21 16:55:50 -0300

    Implementando anotação para que o Spring gerencie a repository com IoC

--- src/main/java/br/com/dev/DesafioItau/repository/TransacaoRepository.java
+++ src/main/java/br/com/dev/DesafioItau/repository/TransacaoRepository.java
@@ -1,8 +1,11 @@
 package br.com.dev.DesafioItau.repository;
 
+import org.springframework.stereotype.Repository;
+
 import br.com.dev.DesafioItau.domain.core.Transacao;
 import br.com.dev.DesafioItau.repository.core.CrudMemoryRepository;
 
+@Repository
 public class TransacaoRepository extends CrudMemoryRepository<Transacao, Integer> {
 
     @Override
