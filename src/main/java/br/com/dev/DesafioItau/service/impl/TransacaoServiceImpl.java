9ee91f2ccc0868e94b32fbb9640a0330a8dce427
KauanFM <kauanmendes.5@hotmail.com>
2026-05-21 19:10:32 -0300

    Implementando método de calcular Estatisticas

--- src/main/java/br/com/dev/DesafioItau/service/impl/TransacaoServiceImpl.java
+++ src/main/java/br/com/dev/DesafioItau/service/impl/TransacaoServiceImpl.java
@@ -1,8 +1,13 @@
 package br.com.dev.DesafioItau.service.impl;
 
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import br.com.dev.DesafioItau.domain.core.Transacao;
+import br.com.dev.DesafioItau.domain.dto.EstatisticaDTO;
 import br.com.dev.DesafioItau.domain.dto.TransacaoDTO;
 import br.com.dev.DesafioItau.repository.TransacaoRepository;
 import br.com.dev.DesafioItau.service.iservice.TransacaoService;
@@ -32,6 +37,39 @@ public class TransacaoServiceImpl implements TransacaoService {
     @Override
     public void deletarTransacoes() {
         transacaoRepository.deleteAll();
+    }
+
+    @Override
+    public EstatisticaDTO calcularEstatisticas() {
+
+        List<Transacao> transacoes = transacaoRepository.findAll();
+
+        OffsetDateTime limite = OffsetDateTime.now().minusSeconds(60);
+
+        DoubleSummaryStatistics estatisticas = transacoes.stream()
+                .filter(t -> !t.getDataHora().isBefore(limite))
+                .map(Transacao::getValor)
+                .mapToDouble(Double::doubleValue)
+                .summaryStatistics();
+
+        EstatisticaDTO estatisticaDTO = new EstatisticaDTO();
+        
+        if (estatisticas.getCount() == 0) {
+            estatisticaDTO.setCount(0);
+            estatisticaDTO.setSum(0.0);
+            estatisticaDTO.setAvg(0.0);
+            estatisticaDTO.setMin(0.0);
+            estatisticaDTO.setMax(0.0);
+            return estatisticaDTO;
+        }
+
+        return EstatisticaDTO.builder()
+                .count(((int) estatisticas.getCount()))
+                .sum(estatisticas.getSum())
+                .avg(estatisticas.getAverage())
+                .min(estatisticas.getMin())
+                .max(estatisticas.getMax())
+                .build();
     }
 
 }
--- src/main/java/br/com/dev/DesafioItau/service/iservice/TransacaoService.java
+++ src/main/java/br/com/dev/DesafioItau/service/iservice/TransacaoService.java
@@ -1,10 +1,12 @@
 package br.com.dev.DesafioItau.service.iservice;
 
+import br.com.dev.DesafioItau.domain.dto.EstatisticaDTO;
 import br.com.dev.DesafioItau.domain.dto.TransacaoDTO;
 
 public interface TransacaoService {
 
     void salvarTransacao(TransacaoDTO transacaoDTO);
     void deletarTransacoes();
+    EstatisticaDTO calcularEstatisticas();
 
 }
